c9fbf5dfb447df21905cd5db11d453668180ba3c
Unknown <rinio.nelstein@hva.nl>
2018-12-04 11:34:58 +0100

    App implemented

---
+++ app/src/main/java/com/example/lifesopriceless/myapplication/ItemObject.java
@@ -0,0 +1,11 @@
+package com.example.lifesopriceless.myapplication;
+
+class ItemObject {
+    private String contents;
+    public ItemObject(String contents) {
+        this.contents = contents;
+    }
+    public String getContents() {
+        return contents;
+    }
+}
---
+++ app/src/main/java/com/example/lifesopriceless/myapplication/MainPanelFragment.java
@@ -0,0 +1,20 @@
+package com.example.lifesopriceless.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainPanelFragment  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_panel, container, false);
+        return v;
+    }
+}
