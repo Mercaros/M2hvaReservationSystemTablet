65a791946c4abb0b1440f2eb69115dc738d4ecbe
Unknown <rinio.nelstein@hva.nl>
2018-12-18 13:58:11 +0100

    static app

--- app/src/main/java/com/example/lifesopriceless/myapplication/ChooseRoomActivity.java
+++ app/src/main/java/com/example/lifesopriceless/myapplication/ChooseRoomActivity.java
@@ -8,29 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.example.lifesopriceless.myapplication.repository.RoomRepo;
-
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
-import java.util.List;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 
 import com.example.lifesopriceless.myapplication.viewmodel.ChooseRoomViewModel;
-import com.example.lifesopriceless.myapplication.viewmodel.MainPanelViewModel;
 
 import java.util.List;
+
 
 public class ChooseRoomActivity extends AppCompatActivity {
     private ChooseRoomViewModel mViewModel;
 
-    private RecyclerView roomListView;
-    private List<Room> roomList;
-    private ChooseRoomAdapter chooseRoomAdapter;
-    RoomRepo roomRepo = new RoomRepo(this);
+    private RecyclerView mRecyclerView;
+    private ChooseRoomAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
 
 
     @Override
@@ -38,53 +27,29 @@ public class ChooseRoomActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_room);
-        roomListView = findViewById(R.id.recyclerView);
-        loadData();
-    }
 
-    private void updateUI() {
-        if (chooseRoomAdapter == null) {
-            chooseRoomAdapter = new ChooseRoomAdapter(roomList);
-            roomListView.setLayoutManager(new LinearLayoutManager(ChooseRoomActivity.this));
-            roomListView.setAdapter(chooseRoomAdapter);
-        } else {
-            chooseRoomAdapter.swapList(roomList);
-        }
-    }
-
-    private void loadData() {
-        Observable.just(roomRepo.getList()).subscribe(new Observer<List<Room>>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(List<Room> rooms) {
-            roomList = rooms;
-            }
-
-            @Override
-            public void onError(Throwable e) {
-            }
-
-            @Override
-            public void onComplete() {
-                updateUI();
-            }
-        });
-
+        mRecyclerView = findViewById(R.id.recyclerView);
         mViewModel = ViewModelProviders.of(this).get(ChooseRoomViewModel.class);
         mViewModel.init();
-
-
 
         mViewModel.getRooms().observe(this, new Observer<List<Room>>() {
             @Override
             public void onChanged(@Nullable List<Room> rooms) {
-
+                mAdapter.notifyDataSetChanged();
             }
         });
+
+        initRecyclerView();
+
+
+    }
+
+    private void initRecyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ChooseRoomAdapter(mViewModel.getRooms().getValue(), ChooseRoomActivity.this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
 
